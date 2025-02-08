dc5653f5b652f075824b5687172b9068222df34f
Matt Rowling <matt@rowling.co.nz>
2025-02-08 13:38:55 +1100

    turn into react app

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const App = () => {
+    return (
+        <div>
+            <h1>
+                mattrowling.
+                <a className="small-emoji" href="https://www.hello.kiwi/why-dotkiwi">
+                    🥝
+                </a>
+            </h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
